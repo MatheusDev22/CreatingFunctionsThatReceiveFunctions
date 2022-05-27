5c83864af1c8ce3c74b62f0237b9a0cd36464ad1
MatheusDev22 <matheusbsb95@gmail.com>
2022-05-27 17:43:51 -0300

    Function that takes a lambda function as an argument

--- src/model/service/ProductService.java
+++ src/model/service/ProductService.java
@@ -1,17 +1,21 @@
 package model.service;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import entity.Product;
 
 public class ProductService {
 
-	public double filteredSum(List<Product> list) {
+		
+	public double filteredSum(List<Product> list, Predicate<Product> criteria) {
 		double sum = 0.0;
 		for (Product p : list) {
-			if(p.getName().charAt(0) == 'T');
-			sum += p.getPrice();
+			if (criteria.test(p)) {
+				sum += p.getPrice();
+			}
 		}
-		return sum;
+		return sum; 
 	}
 }
+
